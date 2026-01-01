f34a274b97fac71ac9357114112e31685e183d0f
Denys Zhukov <mr.pap2000@gmail.com>
2026-01-01 02:14:00 +0200

    ref: disable admob

--- Assets/Scripts/Ads/AdInitialize.cs
+++ Assets/Scripts/Ads/AdInitialize.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
-using GoogleMobileAds.Api;
 
 public class AdInitialize : MonoBehaviour
 {
-    private void Awake()
-    {
-        // Initialize the Google Mobile Ads SDK.
-        MobileAds.Initialize((InitializationStatus initStatus) =>
-        {
-            // This callback is called once the MobileAds SDK is initialized.
-        });
-    }
+    //private void Awake()
+    //{
+    //    // Initialize the Google Mobile Ads SDK.
+    //    MobileAds.Initialize((InitializationStatus initStatus) =>
+    //    {
+    //        // This callback is called once the MobileAds SDK is initialized.
+    //    });
+    //}
 }
