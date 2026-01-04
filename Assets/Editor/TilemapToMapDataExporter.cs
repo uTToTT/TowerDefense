a237876bfea869d2ca726e72ebca3441fe8f86db
Denys Zhukov <mr.pap2000@gmail.com>
2026-01-04 12:31:26 +0200

    fix: fix map generation size

--- Assets/Editor/TilemapToMapDataExporter.cs
+++ Assets/Editor/TilemapToMapDataExporter.cs
@@ -1,5 +1,4 @@
 using UnityEditor;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,48 +7,67 @@ public class TilemapToMapDataExporter
     [MenuItem("TD/Export Map")]
     public static void Export()
     {
-        var path = GameObject.Find("PathTilemap").GetComponent<Tilemap>();
-        var build = GameObject.Find("BuildableTilemap").GetComponent<Tilemap>();
-        var block = GameObject.Find("BlockedTilemap").GetComponent<Tilemap>();
+        var path = GameObject.Find("PathTilemap")?.GetComponent<Tilemap>();
+        var build = GameObject.Find("BuildableTilemap")?.GetComponent<Tilemap>();
+        var block = GameObject.Find("BlockedTilemap")?.GetComponent<Tilemap>();
 
-        var pathBounds = path.cellBounds;
-        var buildBounds = build.cellBounds;
-        var blockBounds = block.cellBounds;
+        Tilemap[] maps = { path, build, block };
 
-        int xMin = Mathf.Min(pathBounds.xMin, buildBounds.xMin, blockBounds.xMin);
-        int yMin = Mathf.Min(pathBounds.yMin, buildBounds.yMin, blockBounds.yMin);
+        int xMin = int.MaxValue;
+        int yMin = int.MaxValue;
+        int xMax = int.MinValue;
+        int yMax = int.MinValue;
 
-        int xMax = Mathf.Max(pathBounds.xMax, buildBounds.xMax, blockBounds.xMax);
-        int yMax = Mathf.Max(pathBounds.yMax, buildBounds.yMax, blockBounds.yMax);
-
-        Debug.Log($"pathBounds{}");
-
-        var mapData = ScriptableObject.CreateInstance<MapData>();
-
-        mapData.width = xMax - xMin;
-        mapData.height = yMax - yMin;
-
-        mapData.cells = new CellType[mapData.width * mapData.height];
-
-        var bounds = path.cellBounds;
-
-        for (int y = 0; y < mapData.height; y++)
+        foreach (var map in maps)
         {
-            for (int x = 0; x < mapData.width; x++)
-            {
-                var pos = new Vector3Int(xMin + x, yMin + y, 0);
+            if (map == null) continue;
 
-                if (path.HasTile(pos))
-                    mapData.cells[y * mapData.width + x] = CellType.Path;
-                else if (build.HasTile(pos))
-                    mapData.cells[y * mapData.width + x] = CellType.Buildable;
-                else
-                    mapData.cells[y * mapData.width + x] = CellType.Blocked;
+            foreach (var pos in map.cellBounds.allPositionsWithin)
+            {
+                if (!map.HasTile(pos)) continue;
+
+                xMin = Mathf.Min(xMin, pos.x);
+                yMin = Mathf.Min(yMin, pos.y);
+                xMax = Mathf.Max(xMax, pos.x + 1);
+                yMax = Mathf.Max(yMax, pos.y + 1);
             }
         }
 
+        if (xMin == int.MaxValue)
+        {
+            Debug.LogError("No tiles found on any Tilemap");
+            return;
+        }
 
-        AssetDatabase.CreateAsset(mapData, "Assets/!TowerDefense/!Data/Maps/NewMap.asset");
+        int width = xMax - xMin;
+        int height = yMax - yMin;
+
+        var mapData = ScriptableObject.CreateInstance<MapData>();
+        mapData.width = width;
+        mapData.height = height;
+        mapData.cells = new CellType[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                var pos = new Vector3Int(xMin + x, yMin + y, 0);
+
+                if (path != null && path.HasTile(pos))
+                    mapData.cells[y * width + x] = CellType.Path;
+                else if (build != null && build.HasTile(pos))
+                    mapData.cells[y * width + x] = CellType.Buildable;
+                else
+                    mapData.cells[y * width + x] = CellType.Blocked;
+            }
+        }
+
+        AssetDatabase.CreateAsset(
+            mapData,
+            "Assets/!TowerDefense/!Data/Maps/NewMap.asset"
+        );
         AssetDatabase.SaveAssets();
+
+        Debug.Log($"Map exported: {width}x{height}");
     }
 }
