365d0bc4621abeea65ffdf4ff9d5bbbdae3c368a
Denys Zhukov <mr.pap2000@gmail.com>
2026-01-21 23:51:03 +0200

    ref

--- Assets/TEST.cs
+++ Assets/TEST.cs
@@ -5,24 +5,24 @@ public class TEST : MonoBehaviour
 {
     private void Awake()
     {
-        for (int i = 1; i <= SceneManager.sceneCountInBuildSettings; i++)
-        {
-            PlayerPrefs.SetInt("StarsForLevel" + i, 3);
-            Debug.Log(PlayerPrefs.GetInt("StarsForLevel" + i));
-        }
+        //for (int i = 1; i <= SceneManager.sceneCountInBuildSettings; i++)
+        //{
+        //    PlayerPrefs.SetInt("StarsForLevel" + i, 3);
+        //    Debug.Log(PlayerPrefs.GetInt("StarsForLevel" + i));
+        //}
 
-        int _starCounter = 0;
+        //int _starCounter = 0;
 
-        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
-        {
-            _starCounter += PlayerPrefs.GetInt("StarsForLevel" + (i + 1));
-        }
+        //for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        //{
+        //    _starCounter += PlayerPrefs.GetInt("StarsForLevel" + (i + 1));
+        //}
 
-        PlayerPrefs.SetInt("StarCount", _starCounter);
+        //PlayerPrefs.SetInt("StarCount", _starCounter);
 
-        Debug.Log(PlayerPrefs.GetInt("StarsForLevel" + 50));
+        //Debug.Log(PlayerPrefs.GetInt("StarsForLevel" + 50));
 
-        PlayerPrefs.SetInt("RemoveAds", 0);
+        //PlayerPrefs.SetInt("RemoveAds", 0);
     }
 
     public void DeleteAllPlayerPrefs()
